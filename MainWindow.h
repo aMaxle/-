1e7d55a1910b68c27ff22fe2c8048fe5bc90e534
aMaxle <70522664+aMaxle@users.noreply.github.com>
2020-09-28 23:27:56 +0800

    Update MainWindow.h

--- MainWindow.h
+++ MainWindow.h
@@ -1,5 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "Budget.h"
 
 #include <QMainWindow>
 #include <QPushButton>
@@ -7,6 +8,13 @@
 #include <QString>
 #include <QDialog>
 #include <QMessageBox>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
+
+using namespace QtCharts;
 
 namespace Ui {
 class MainWindow;
@@ -16,12 +24,31 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 private:
+//    struct M{
+//        double moneyType=0;
+//        QString TYPE;
+//        double percentage;
+//    }m[100];
     QString InType;
+
+    double budgetMoney;
+
     struct Bill{
         QString money;
         QString type;
         QDate date;
+        QString remarks;
+        bool moneyplus=false;
     }bill[100];
+
+    //Ui::MainWindow *ui;
+    //QAction *action;
+    //QMenu *menu;
+
+    //void actions();
+
+    Budget win;
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
@@ -29,10 +56,18 @@ public:
     QDialog dlg;
 
     void getMoney();
+
     void getType();
+
     void getInType();
-    void getbill();
+
+    //void getbill();
+
     void DouStr();
+
+    void save_monthMoney(double);
+
+    void cancel();
 
 private slots:
     void on_book_2_clicked();
@@ -45,11 +80,20 @@ private slots:
 
     void on_book_clicked();
 
-    void show_bill();
+    //void show_bill();
 
     void on_clear_onetype_clicked();
 
     void on_Price_Sort_clicked();
+
+    void on_Time_Sort_clicked();
+
+    //void contextMenuEvent(QContextMenuEvent *event);
+
+    //void actionClick();
+
+    //void changeWin();
+
 
 private:
     Ui::MainWindow *ui;
