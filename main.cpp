39b89614d03b519546f5f838a7a2a2981851d188
aMaxle <70522664+aMaxle@users.noreply.github.com>
2020-09-28 23:28:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "MainWindow.h"
+#include "Budget.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    Budget win;
+    w.setWindowTitle("记账本");
+    //w.resize(570,700);
+    w.move(650,220);
+    //w.show();
+
 
     return a.exec();
 }
